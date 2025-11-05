522780db62101b092fd50d44b5b05cf1e4468995
Maoz1005 <maozhaim@mail.tau.ac.il>
2025-11-05 20:19:10 +0200

    TokenNames final version

--- src/TokenNames.java
+++ src/TokenNames.java
@@ -1,12 +1,37 @@
 public interface TokenNames {
   /* terminals */
   public static final int EOF = 0;
-  public static final int PLUS = 1;
-  public static final int MINUS = 2;
-  public static final int TIMES = 3;
-  public static final int DIVIDE = 4;
-  public static final int LPAREN = 5;
-  public static final int RPAREN = 6;
-  public static final int NUMBER = 7;
-  public static final int ID = 8;
+  public static final int LPAREN = 1;
+  public static final int RPAREN = 2;
+  public static final int LBRACK = 3;
+  public static final int RBRACK = 4;
+  public static final int LBRACE = 5;
+  public static final int RBRACE = 6;
+  public static final int PLUS = 7;
+  public static final int MINUS = 8;
+  public static final int TIMES = 9;
+  public static final int DIVIDE = 10;
+  public static final int COMMA = 11;
+  public static final int DOT = 12;
+  public static final int SEMICOLON = 13;
+  public static final int TYPE_INT = 14;
+  public static final int TYPE_STRING = 15;
+  public static final int TYPE_VOID = 16;
+  public static final int ASSIGN = 17;
+  public static final int EQ = 18;
+  public static final int LT = 19;
+  public static final int GT = 20;
+  public static final int ARRAY = 21;
+  public static final int CLASS = 22;
+  public static final int RETURN = 23;
+  public static final int WHILE = 24;
+  public static final int IF = 25;
+  public static final int ELSE = 26;
+  public static final int NEW = 27;
+  public static final int EXTENDS = 28;
+  public static final int NIL = 29;
+  public static final int INT = 30; // INT(value) - value is an int
+  public static final int STRING = 31; // STRING(value) - value is a string
+  public static final int ID = 32; // ID(value) - value is an id
 }
+
