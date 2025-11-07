112a8bf18fe398ad5873837026a053b875d597c4
Maoz1005 <maozhaim@mail.tau.ac.il>
2025-11-07 15:38:09 +0200

    TokenNames final version

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,45 @@ import java_cup.runtime.Symbol;
    
 public class Main
 {
+	final static int MAX_NUMBER = (int)Math.pow(2,15) - 1;  // Maximal int
+
+	public static final String[] id_to_name = { /* Array mapping token IDs to their names */
+			"EOF",          // 0
+			"LPAREN",       // 1
+			"RPAREN",       // 2
+			"LBRACK",       // 3
+			"RBRACK",       // 4
+			"LBRACE",       // 5
+			"RBRACE",       // 6
+			"PLUS",         // 7
+			"MINUS",        // 8
+			"TIMES",        // 9
+			"DIVIDE",       // 10
+			"COMMA",        // 11
+			"DOT",          // 12
+			"SEMICOLON",    // 13
+			"TYPE_INT",     // 14
+			"TYPE_STRING",  // 15
+			"TYPE_VOID",    // 16
+			"ASSIGN",       // 17
+			"EQ",           // 18
+			"LT",           // 19
+			"GT",           // 20
+			"ARRAY",        // 21
+			"CLASS",        // 22
+			"RETURN",       // 23
+			"WHILE",        // 24
+			"IF",           // 25
+			"ELSE",         // 26
+			"NEW",          // 27
+			"EXTENDS",      // 28
+			"NIL",          // 29
+			"INT",       	// 30
+			"STRING",       // 31
+			"ID",           // 32
+			"ERROR"			// 33
+	};
+
 	static public void main(String argv[])
 	{
 		Lexer l;
