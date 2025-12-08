e264874c48c6a1bd4f36c27c5163cf8cd6ebd57d
Maoz1005 <maozhaim@mail.tau.ac.il>
2025-12-08 15:10:25 +0200

    Updated classes - cfield, cfieldfunc, cfieldvar, classdec, exp

--- ex3/src/ast/AstCfieldFunc.java
+++ ex3/src/ast/AstCfieldFunc.java
@@ -1,23 +1,30 @@
 package ast;
 
+import types.Type;
 import java.util.Arrays;
 import java.util.List;
 
 public class AstCfieldFunc extends AstCfield{
     public AstFuncdec funcDec;
 
-    public AstCfieldFunc(AstFuncdec funcDec) {
-        super("CField -> funcDec");
+    public AstCfieldFunc(AstFuncdec funcDec, int lineNum) {
+        super("CField -> funcDec", lineNum);
         this.funcDec = funcDec;
     }
+
 
     @Override
     protected List<? extends AstNode> GetChildren() {
         return Arrays.asList(funcDec);
     }
 
+
     @Override
     protected String GetNodeName() {
         return "CFIELD\nFUNCDEC";
     }
+
+
+    @Override
+    public Type SemantMe() { return funcDec.SemantMe(); }
 }
--- ex3/src/ast/AstExp.java
+++ ex3/src/ast/AstExp.java
@@ -5,4 +5,8 @@ public abstract class AstExp extends AstNode
     public AstExp(String derivation, int lineNum) {
 		super(derivation, lineNum);
 	}
+
+	// to be overridden:
+	public boolean isConstant() { return false; }
+	public boolean isNewExp() {return false; }
 }
